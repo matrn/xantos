516cf1c25e6306dd8c6b98baa22ca639e9de86c7
matrn <trnka.matej@seznam.cz>
2019-12-29 17:46:39 +0100

    updates

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Creating virtual environment.."
-virtualenv --python=/usr/bin/python3.7 --no-site-packages --distribute .env
+virtualenv --python=/usr/bin/python3.7 --no-site-packages --distribute .venv
 
 echo "Setting up permissions.."
 chmod +x xantos
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source .env/bin/activate
+source .venv/bin/activate
 pip install -r requirements.txt
